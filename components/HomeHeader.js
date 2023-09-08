df9c5417aea6baa21948c2051881e358847b694c
aa6622 <kufupa@users.noreply.github.com>
2023-09-08 17:39:29 +0100

    changed onChangeText prop to be a callback func that is passed into HomeHeader.js

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -2,7 +2,7 @@ import { View, Text, Image, TextInput } from 'react-native'
 
 import {COLORS, FONTS, SIZES, assets} from '../constants'
 
-const HomeHeader = () => {
+const HomeHeader = ({onSearch}) => {
   return (
     <View style={{
       backgroundColor: COLORS.primary,
@@ -66,7 +66,7 @@ const HomeHeader = () => {
           <TextInput
             placeHolder="Search NFTs"
             style={{flex: 1}}
-            onChangeText={() => {}}
+            onChangeText={onSearch}
           />
         </View>
 
