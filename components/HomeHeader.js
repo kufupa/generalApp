fccd7f786b3ff223dc5616d3640a246e72e1e1cb
aa6622 <kufupa@users.noreply.github.com>
2023-09-08 16:45:30 +0100

    top left logo and top right person image added

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native'
+
+import {COLORS, FONTS, SIZES, assets} from '../constants'
 
 const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{
+      backgroundColor: COLORS.primary,
+      padding: SIZES.font
+    }}>
+
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        <Image 
+          source={assets.logo }
+          resizeMode="contain"
+          style={{width:90, height:25}}
+        />
+
+        <View style={{width:45, height:45 }}>
+          <Image 
+            source={assets.person01}
+            resizeMode="contain"
+            style={{width:"100%", height:"100%"}} // Make sure image fits in area
+          />
+
+          {/* V small blue checkmark on bottom right of image */}
+          <Image 
+            source={assets.badge}
+            resizeMode="contain"
+            // Bottom:0 and right:0 position it to bottom right 
+            style={{position: "absolute", width:15, height:15, bottom: 0, right:0}} 
+          />
+        </View>
+      </View>
+
     </View>
   )
 }
