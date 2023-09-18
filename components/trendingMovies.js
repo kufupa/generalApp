58410bfa2f9dbb3c6c6bbcb0ff6e82f036450ca8
aa6622 <kufupa@users.noreply.github.com>
2023-09-18 13:02:05 +0100

    init with margin botton of 8

---
+++ components/trendingMovies.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const trendingMovies = () => {
+  return (
+    <View class-name="mb-8">
+      <Text>trendingMovies</Text>
+    </View>
+  )
+}
+
+export default trendingMovies
