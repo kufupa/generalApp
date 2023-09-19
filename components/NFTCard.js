a0ae99d189c6d284ce657579b50755986f3325c8
aa6622 <kufupa@users.noreply.github.com>
2023-09-20 00:15:30 +0100

    Font error solved by expo fixing versions

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
-import {COLORS, SIZES, SHADOWS, assets} from '../constants'
+import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../constants'
 import { CircleButton, RectButton } from './Button'
 import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
 
