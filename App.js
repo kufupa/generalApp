879859e8c9faf662c129ff5960568c027eae061f
Siddhant Chauhan <sc3722@ic.ac.uk>
2023-09-20 23:45:45 +0530

    Fixed MainContainer and reorganized files

--- App.js
+++ App.js
@@ -1,25 +1,5 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-ionicons';
-
-import Home from './navigation/screens/Home';
-import Details from './navigation/screens/Details';
-import ProfileScreen from './navigation/screens/ProfileScreen';
-import SettingsScreen from './navigation/screens/SettingsScreen';
 import MainContainer from './navigation/MainContainer';
-
-// const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "transparent"
-  }
-}
 
 const App = () => {
 
@@ -34,44 +14,8 @@ const App = () => {
   // Fonts not loaded for some reason
   if(!loaded) return null;
 
-
   return (
-    // <NavigationContainer theme={theme}>
-    //   <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-    //     <Stack.Screen name='Home' component={Home}/>
-    //     <Stack.Screen name='Details' component={Details}/>
-    //     <Stack.Screen name='Profile' component={ProfileScreen}/>
-
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <NavigationContainer theme={theme}>
-        <Tab.Navigator
-        initialRouteName={'Home'}
-        screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-                let iconName;
-                let rn = route.name;
-
-                if (rn == 'Home') {
-                    iconName = focused ? 'home' : 'home-outline'
-                } else if (rn == 'Details') {
-                    iconName = focused ? 'list' : 'list-outline'
-                } else if (rn == 'Settings') {
-                    iconName = focused ? 'settings' : 'settings-outline'
-                }
-
-                return <Icon name={iconName} size={size} color={color}/>
-            },
-        })}
-        >
-            <Tab.Screen name={'Home'} component={Home}/>
-            <Tab.Screen name={'Details'} component={Details}/>
-            <Tab.Screen name={'Profile'} component={ProfileScreen}/>
-            <Tab.Screen name={'Settings'} component={SettingsScreen}/>
-
-        </Tab.Navigator>
-      </NavigationContainer>
-    
+    <MainContainer></MainContainer>
   );
 }
 
--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-ionicons';
 
@@ -10,40 +10,42 @@ import Details from './screens/Details';
 import ProfileScreen from './screens/ProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
-// Screen Names
-const homeName = 'Home';
-const detailsName = 'Details';
-const profileName = 'Profile';
-const settingsScreen = 'Settings';
-
 const Tab = createBottomTabNavigator();
 
+const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: "transparent"
+    }
+  }
 
 const MainContainer = () => {
     return (
-      <NavigationContainer theme={theme}>
+        <NavigationContainer theme={theme}>
         <Tab.Navigator
-        initialRouteName={homeName}
-        screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-                let iconName;
-                let rn = route.name;
+        initialRouteName={'Home'}
+        // screenOptions={({route}) => ({
+        //     tabBarIcon: ({focused, color, size}) => {
+        //         let iconName;
+        //         let rn = route.name;
 
-                if (rn == homeName) {
-                    iconName = focused ? 'home' : 'home-outline'
-                } else if (rn == detailsName) {
-                    iconName = focused ? 'list' : 'list-outline'
-                } else if (rn == settingsScreen) {
-                    iconName = focused ? 'settings' : 'settings-outline'
-                }
+        //         if (rn == 'Home') {
+        //             iconName = focused ? 'home' : 'home-outline'
+        //         } else if (rn == 'Details') {
+        //             iconName = focused ? 'list' : 'list-outline'
+        //         } else if (rn == 'Settings') {
+        //             iconName = focused ? 'settings' : 'settings-outline'
+        //         }
 
-                return <Icon name={iconName} size={size} color={color}/>
-            },
-        })}>
-            <Tab.Screen name={homeName} component={Home}/>
-            <Tab.Screen name={detailsName} component={Details}/>
-            <Tab.Screen name={profileName} component={ProfileScreen}/>
-            <Tab.Screen name={settingsScreen} component={SettingsScreen}/>
+        //         return <Icon name={iconName} size={size} color={color}/>
+        //     },
+        // })}
+        >
+            <Tab.Screen name={'Home'} component={Home}/>
+            <Tab.Screen name={'Details'} component={Details}/>
+            <Tab.Screen name={'Profile'} component={ProfileScreen}/>
+            <Tab.Screen name={'Settings'} component={SettingsScreen}/>
 
         </Tab.Navigator>
       </NavigationContainer>
