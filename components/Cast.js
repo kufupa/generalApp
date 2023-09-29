e5ea2a7f4fde8692143c104ef7f7f36cc6b1ccb6
Kufupa <WilsonianProgrammer@gmail.com>
2023-09-29 23:58:43 +0100

    changing tailwindCSS to inline styling

--- components/Cast.js
+++ components/Cast.js
@@ -11,8 +11,19 @@ import { fallbackPersonImage, image185, image342 } from "../api/moviedb";
 
 export default function Cast({ cast, navigation }) {
   return (
-    <View className="my-6">
-      <Text className="text-white text-lg mx-4 mb-5">Top Cast</Text>
+    <View
+      style={{
+        marginVertical: 6,
+      }}>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 15,
+          marginHorizontal: 4,
+          marginBottom: 5,
+        }}>
+        Top Cast
+      </Text>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
@@ -23,10 +34,26 @@ export default function Cast({ cast, navigation }) {
               <TouchableOpacity
                 key={index}
                 onPress={() => navigation.navigate("Person", person)}
-                className="mr-4 items-center">
-                <View className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
+                style={{
+                  marginRight: 4,
+                  align: "center",
+                }}>
+                <View
+                  style={{
+                    overflow: "hidden",
+                    borderRadius: "50%", // 'rounded-full' corresponds to a full circle
+                    height: "20px", // You may need to adjust the size as needed
+                    width: "20px", // You may need to adjust the size as needed
+                    display: "flex", // 'items-center' centers the content horizontally
+                    alignItems: "center", // 'items-center' centers the content vertically
+                    border: "1px solid #CCCCCC", // 'border' and 'border-neutral-500' set the border styles
+                  }}>
                   <Image
-                    className="rounded-2xl h-24 w-20"
+                    style={{
+                      borderRadius: "1rem",
+                      height: "24px",
+                      width: "20px",
+                    }}
                     source={require("../assets/images/castImage1.png")}
                     // source={{
                     //   uri:
